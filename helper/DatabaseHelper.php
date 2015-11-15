1c57bfea423f19b870333a251e761d326119f405
hswie <hswiecioch@hotmail.com>
2015-11-15 18:24:34 +0100

    addTask module

--- helper/DatabaseHelper.php
+++ helper/DatabaseHelper.php
@@ -76,6 +76,42 @@ class DatabaseHelper {
         mysqli_close($connection);
     }
     
+    public function getFamilyList(){
+        
+        require('conf/config.php');
+       
+        try {
+            // Create connection
+           $connection = new mysqli($host, $username, $password, $dbname);
+           // Check connection
+           if ($connection->connect_error) {
+               die("Connection failed: " . $connection->connect_error);
+           } 
+           
+          $sql = "SELECT * FROM family";
+          $carerrray = array();
+         
+           if($result = $connection->query($sql)){
+          
+            while($row = $result->fetch_array(MYSQL_ASSOC)){
+            $carerrray[] = $row;
+            }          
+          }
+            
+          die(json_encode($carerrray));
+          
+        } catch (PDOException $ex) {
+	
+		
+        $response['success'] = 0 ;
+        $response['message'] = "Database Error1, Please try Again";
+        die(json_encode($response));
+       
+        }
+        $result->close();
+        mysqli_close($connection);
+    }
+    
     public function getCarerTasks($carerID){
         
         require('conf/config.php');
